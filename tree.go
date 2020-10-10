ee9807df050743eb8e1af45dba0330112ed5e774
Luis Ángel Méndez Gort <mendez@talon.one>
2020-10-10 13:43:24 +0200

    alternative binary tree insertion

--- tree.go
+++ tree.go
@@ -32,3 +32,33 @@ func Insert(a *Tree, v int) {
 func Leaf(v int) *Tree {
 	return &Tree{Value: v, Empty: false}
 }
+
+type BNode struct {
+	Value int
+	Empty bool
+	Left  uint
+	Right uint
+}
+
+type BTree struct {
+	Nodes []BNode
+}
+
+func NewBTree() *BTree {
+	return &BTree{Nodes: []BNode{{Empty: true}}}
+}
+
+func (b *BTree) Insert(v int) {
+	current := &b.Nodes[0]
+	for !current.Empty {
+		if v < current.Value {
+			current = &b.Nodes[current.Left]
+		} else {
+			current = &b.Nodes[current.Right]
+		}
+	}
+	current.Value, current.Empty = v, false
+	current.Left = uint(len(b.Nodes))
+	current.Right = uint(len(b.Nodes)) + 1
+	b.Nodes = append(b.Nodes, BNode{Empty: true}, BNode{Empty: true})
+}
--- tree_test.go
+++ tree_test.go
@@ -39,3 +39,41 @@ func TestInsert(t *testing.T) {
 		require.Equal(t, j.expected, a, "At %d", i)
 	}
 }
+
+func TestBInsert(t *testing.T) {
+	ts := []struct {
+		xs       []int
+		expected *BTree
+	}{
+		{
+			xs: []int{8, 10, 9, 11, 5, 6, 3},
+			expected: &BTree{
+				Nodes: []BNode{
+					{Value: 8, Left: 1, Right: 2},
+					{Value: 5, Left: 9, Right: 10},
+					{Value: 10, Left: 3, Right: 4},
+					{Value: 9, Left: 5, Right: 6},
+					{Value: 11, Left: 7, Right: 8},
+					{Empty: true},
+					{Empty: true},
+					{Empty: true},
+					{Empty: true},
+					{Value: 3, Left: 13, Right: 14},
+					{Value: 6, Left: 11, Right: 12},
+					{Empty: true},
+					{Empty: true},
+					{Empty: true},
+					{Empty: true},
+				},
+			},
+		},
+	}
+	for i, j := range ts {
+		b := NewBTree()
+		for _, k := range j.xs {
+			b.Insert(k)
+		}
+		t.Logf("%v", b.Nodes)
+		require.Equal(t, j.expected, b, "At %d", i)
+	}
+}
